ba9c68350bf580b81110e0ace22fa77eee74dbae
Clément <illuclem@gmail.com>
2017-02-13 19:10:46 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,9 @@
-var xat        = require('xatlib');
+var xat = require('xatlib');
 
-//Ouput : SlOom
-xat.getRegname(110110, function(response) {
-    console.log(response);
-});
-
-//Ouput : Not found
-xat.getID('xSlOom444', function(response) {
-    console.log(response);
-});
-
-//Ouput : { id: '3764264', Name: 'Abc', Desc: 'Welcome to Abc Chatgroup! Enjoy your stay! :)', Cinfo:  { Background: 'http://i55.tinypic.com/se2hwm.png', Language: 'English', Radio: 'http://173.242.119.50:9996', Buttons: 'None' } }
-xat.getChatInfo('abc', function(response) {
-    console.log(response);
+xat.getRegname('110110', (err, res) => {
+	if (err != null) {
+		console.log('Something went wrong with your request : ' + err);
+	} else {
+		console.log(res);
+	}
 });
