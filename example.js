4156435b4a5ae83c889bc41a32ff84709069edeb
Clément <illuclem@gmail.com>
2017-02-13 19:11:16 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,9 +1,10 @@
 var xat = require('xatlib');
 
 xat.getRegname('110110', (err, res) => {
-	if (err != null) {
-		console.log('Something went wrong with your request : ' + err);
-	} else {
-		console.log(res);
-	}
+    if (err != null) {
+		  console.log('Something went wrong with your request : ' + err);
+	  } else {
+		  console.log(res);
+	  }
 });
+    
