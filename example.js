112547b721b415fa32a081a7bde5172cca9328d1
Clément <illuclem@gmail.com>
2017-02-14 21:19:16 +0100

    Update example.js

--- example.js
+++ example.js
@@ -3,35 +3,35 @@ const xat = require('xatapi');
 // getRegname example
 xat.getRegname(110110, (err, res) => {
     if (err != null) {
-		console.log('Something went wrong with your request : ' + err);
+	console.log('Something went wrong with your request : ' + err);
     } else {
-		console.log(res);
+	console.log(res);
     }
 });
 
 // getID example
 xat.getID('xSlOom', (err, res) => {
     if (err != null) {
-		console.log('Something went wrong with your request : ' + err);
+	console.log('Something went wrong with your request : ' + err);
     } else {
-		console.log(res);
+	console.log(res);
     }
 });
 
 // get Chat infos
 xat.getChatInfo('rubyyy', (err, res) => {
     if (err != null) {
-		console.log('Something went wrong with your request : ' + err);
+	console.log('Something went wrong with your request : ' + err);
     } else {
-		console.log(res);
+	console.log(res);
     }
 });
 
 // get power infos
 xat.getNewInfo((err, res) => {
     if (err != null) {
-		console.log('Something went wrong with your request : ' + err);
+	console.log('Something went wrong with your request : ' + err);
     } else {
-		console.log(res);
+	console.log(res);
     }
 });
