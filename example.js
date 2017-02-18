caa2d7a9e34cd2c81c6243727fab5ff24862116f
Clément <illuclem@gmail.com>
2017-02-18 14:11:56 +0100

    Add files via upload

--- example.js
+++ example.js
@@ -1,37 +1,46 @@
-const xat = require('xatapi');
+const xat = require('./index.js');
 
 // getRegname example
 xat.getRegname(110110, (err, res) => {
     if (err != null) {
-	console.log('Something went wrong with your request : ' + err);
+		console.log('Something went wrong with your request : ' + err);
     } else {
-	console.log(res);
+		console.log(res);
     }
 });
 
 // getID example
 xat.getID('xSlOom', (err, res) => {
     if (err != null) {
-	console.log('Something went wrong with your request : ' + err);
+		console.log('Something went wrong with your request : ' + err);
     } else {
-	console.log(res);
+		console.log(res);
     }
 });
 
 // get Chat infos
 xat.getChatInfo('rubyyy', (err, res) => {
     if (err != null) {
-	console.log('Something went wrong with your request : ' + err);
+		console.log('Something went wrong with your request : ' + err);
     } else {
-	console.log(res);
+		console.log(res);
     }
 });
 
 // get power infos
 xat.getNewInfo((err, res) => {
     if (err != null) {
-	console.log('Something went wrong with your request : ' + err);
+		console.log('Something went wrong with your request : ' + err);
     } else {
-	console.log(res);
+		console.log(res.id + " is the latest ID!");
+    }
+});
+
+// get chat infos connection
+xat.getChatConnection(5, (err, res) => {
+    if (err != null) {
+		console.log('Something went wrong with your request : ' + err);
+    } else {
+		console.log(res);
     }
 });
