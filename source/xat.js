670405fb81aa527615ce2deb4d05088eb577ebe1
Clément <illuclem@gmail.com>
2017-02-20 13:21:13 +0100

    Changed some things
    Changed the way of JSON.parse to handle easily all errors.

--- source/xat.js
+++ source/xat.js
@@ -9,15 +9,17 @@ const adminsN	= { 'Darren': 7, 'Xat': 42, 'ChrisRixon': 99, 'Sam': 100, 'Chris':
 
 exports.getRegname = (id, callback) => {
     if ((id == undefined) || (id == '') || (isNaN(id))) {
-        return setImmediate(() => callback(new Error("You must specify an ID in your request or ID must be numeric.")));
+        return setImmediate(() =>
+            callback(new Error("You must specify an ID in your request or ID must be numeric.")));
     }
     if (admins[id] !== undefined) {
-        return setImmediate(() => callback(null, admins[id]));
+        return setImmediate(() =>
+            callback(null, admins[id]));
     }
 
     request("http://xat.me/SlOom?id=" + id, function(error, response, body) {
         if (error) {
-          return callback(error)
+          return callback(error);
         }
         if ((body == "") || (body.substr(0, 1) == "<")) {
             callback(new Error("ID not found"));
@@ -29,10 +31,12 @@ exports.getRegname = (id, callback) => {
 
 exports.getID = (reg, callback) => {
     if ((reg == undefined) || (reg == '') || (!isNaN(reg))) {
-        return setImmediate(() => callback(new Error("You must specify a regname in your request.")));
+        return setImmediate(() =>
+            callback(new Error("You must specify a regname in your request.")));
     }
     if (adminsN[reg] !== undefined) {
-        return setImmediate(() => callback(null, adminsN[reg]));
+        return setImmediate(() =>
+            callback(null, adminsN[reg]));
     }
 
     request("http://xat.me/SlOom?name=" + reg, function(error, response, body) {
@@ -49,7 +53,8 @@ exports.getID = (reg, callback) => {
 
 exports.getChatInfo = (chat, callback) => {
     if ((chat == undefined) || (chat == "")) {
-        return setImmediate(() => callback(new Error("You must add a chat name to your request.")));
+        return setImmediate(() =>
+            callback(new Error("You must add a chat name to your request.")));
     }
 
     var url = "http://xat.com/web_gear/chat/roomid.php?d=" + chat + "&v2";
@@ -84,40 +89,40 @@ exports.getNewInfo = (callback) => {
 	var pawns	= [];
 	var url 	= "http://xat.com/web_gear/chat/pow2.php?Sloom=" + Date.now(); // so we can have the latest page. (shrug)
 	request(url, function(error, response, body) {
-		if ((body == false) || (body.substr(0, 1) !== "[")) {
-			callback(new Error('Unable to read pow2 c:'));
-		} else {
-			var json	= JSON.parse(body);
-			var id		= json[0][1]["id"];
-			var status	= json[0][1]["text"] == "" ? "UNLIMITED" : json[0][1]["text"].replace('[', '').replace(']', '');
-			for (var sm in json[4][1]) { // smileys list
-				if (id == json[4][1][sm]) {
-					topsh.push(sm);
-				}
-			}
-			for (var nm in json[6][1]) { // power name 
-				if (id == json[6][1][nm]) {
-					name = nm;
-				}
-			}
-			for (var pw in json[7][1]) { // pawns name 
-				if (id == json[7][1][pw][0]) {
-					pawns.push("hat#h" + pw);
-				}
-			}
-			callback(null, {
-				'id': id,
-				'name': name,
-				'status': status,
-				'topsh': topsh.length == 0 ? nm : topsh.join(","),
-				'pawns': pawns.length == 0 ? null : pawns.join(",")
-			});
-		}
+        try {
+            var json = JSON.parse(body);
+        } catch (e) {
+            return callback(e);
+        }
+        var id		= json[0][1]["id"];
+        var status	= json[0][1]["text"] == "" ? "UNLIMITED" : json[0][1]["text"].replace('[', '').replace(']', '');
+        for (var sm in json[4][1]) { // smileys list
+            if (id == json[4][1][sm]) {
+                topsh.push(sm);
+            }
+        }
+        for (var nm in json[6][1]) { // power name
+            if (id == json[6][1][nm]) {
+                name = nm;
+            }
+        }
+        for (var pw in json[7][1]) { // pawns name
+            if (id == json[7][1][pw][0]) {
+                pawns.push("hat#h" + pw);
+            }
+        }
+        callback(null, {
+            'id': id,
+            'name': name,
+            'status': status,
+            'topsh': topsh.length == 0 ? nm : topsh.join(","),
+            'pawns': pawns.length == 0 ? null : pawns.join(",")
+        });
 	});
 };
 
 exports.getChatConnection = (chatID, callback) => {
-    if ((chatID == undefined) || (chatID == "")) {
+    if ((chatID == undefined) || (chatID == "") || (isNaN(chatID))) {
         return setImmediate(() =>
           callback(new Error("You must add a chat ID to your request.")));
     }
